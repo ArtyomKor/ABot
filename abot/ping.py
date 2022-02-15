9c94e0c6b16c1ac83190d50072910d115f1988e6
ArttomKor <akorolesckij@gmail.com>
2022-02-15 17:28:20 +0700

    hehe 2

--- abot/ping.py
+++ abot/ping.py
@@ -32,6 +32,22 @@ class Ping(commands.Cog):
             mist_func = True
         if mist_func is True:
             mist_func = False
+            s = message.content
+            l = len(s)
+            integ = []
+            i = 0
+            while i < l:
+                s_int = ''
+                a = s[i]
+                while '0' <= a <= '9':
+                    s_int += a
+                    i += 1
+                    if i < l: a = s[i]
+                    else: break
+                    i += 1
+                    if s_int != '': 
+                        integ.append(int(s_int))
+                    print(integ[0])
         print(message.content)
 
     @commands.Cog.listener()
