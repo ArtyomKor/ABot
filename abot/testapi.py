7e06e6d0455e821aefad40f7375bc33e49ee9018
ArttomKor <akorolesckij@gmail.com>
2022-02-19 22:38:49 +0700

    test modal

---
+++ abot/testapi.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+
+class MyModal(Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.add_item(InputText(label="Short Input", placeholder="Placeholder Test"))
+
+        self.add_item(
+            InputText(
+                label="Longer Input",
+                value="Longer Value\nSuper Long Value",
+                style=discord.InputTextStyle.long,
+            )
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="Your Modal Results", color=discord.Color.random())
+        embed.add_field(name="First Input", value=self.children[0].value, inline=False)
+        embed.add_field(name="Second Input", value=self.children[1].value, inline=False)
+        await interaction.response.send_message(embeds=[embed])
+        
+class Test(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+		
+    @commands.slash_command(name="testmodal", guild_ids=[940119996477095986])
+    async def test(self, ctx):
+        modal = MyModal(title="Это ABot, детка!")
+        await ctx.interaction.response.send_modal(modal)
+       
+def setup(bot):
+    bot.add_cog(Test(bot))
