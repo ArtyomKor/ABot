6bd3fc484e37e31a7350b88a373e7d8dd563ba3b
ArttomKor <akorolesckij@gmail.com>
2022-02-19 23:42:47 +0700

    test select

--- abot/testapi.py
+++ abot/testapi.py
@@ -2,6 +2,19 @@ import discord
 from discord.ext import commands
 from discord.ui import Modal, InputText
 
+class MySelect(discord.ui.SelectMenu):
+    def __init__(self):
+        options = [
+            discord.SelectOption(label='Red', description='Your favourite colour is red', emoji='🟥'),
+            discord.SelectOption(label='Green', description='Your favourite colour is green', emoji='🟩'),
+            discord.SelectOption(label='Blue', description='Your favourite colour is blue', emoji='🟦')
+        ]
+        super().__init__(placeholder='Pick your colour', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction):
+        await interaction.response.send_message(f'Your favourite colour is {self.values[0]}', ephemeral=True)
+
+
 class MyModal(Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -31,5 +44,11 @@ class Test(commands.Cog):
         modal = MyModal(title="Это ABot, детка!")
         await ctx.interaction.response.send_modal(modal)
        
+    @commands.slash_command(name="testselect", guild_ids=[940119996477095986])
+    async def select(ctx):
+        view = discord.ui.View()
+        view.add_item(MySelect())
+        await ctx.respond(content="bebra", view=view)
+
 def setup(bot):
     bot.add_cog(Test(bot))
